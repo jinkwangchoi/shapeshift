1ceb81f8ed9db0637fc64cdb0192ebcd6bb8a1ce
Jinkwang Choi <jinkwang.choi@gmail.com>
2017-05-21 21:37:22 +0900

    withdraw

--- shapeshift.go
+++ shapeshift.go
@@ -47,7 +47,7 @@ type RecentTranxResponse []struct {
 	Error
 }
 
-type DepositStatusResponse struct {
+type DepositStatnotInProgressusResponse struct {
 	Status       string          `json:"status"`
 	Address      string          `json:"address"`
 	Withdraw     string          `json:"withdraw,omitempty"`
